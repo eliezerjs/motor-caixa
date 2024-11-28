b6a93dec2ddba01ab7a34df1e9800fd6aa31b645
Eliezer <eliezerjs@live.com>
2024-11-27 21:43:09 -0300

    ajuste prestamista.

--- IntegraCVP/src/IntegraCVP.Application/Interfaces/IPrestaMistaService.cs
+++ IntegraCVP/src/IntegraCVP.Application/Interfaces/IPrestaMistaService.cs
@@ -1,9 +1,12 @@
+using IntegraCVP.Application.Enums;
+using Microsoft.AspNetCore.Http;
+
 namespace IntegraCVP.Application.Interfaces
 {
     public interface IPrestamistaService
-    {
-        byte[] GerarBoasVindasPdf(Dictionary<string, string> dados);
-        byte[] GerarBoasVindasQuinzeP1Pdf(Dictionary<string, string> dados);
-        byte[] GerarBoasVindasQuinzeP2Pdf(Dictionary<string, string> dados);
+    {        
+        byte[] GerarDocumentoPrestamista(Dictionary<string, string> dadosBoleto, PrestamistaType tipo);
+        Task<byte[]> ConverterEGerarPrestamistaPdfAsync(IFormFile file, PrestamistaType tipo);
+
     }
 }
