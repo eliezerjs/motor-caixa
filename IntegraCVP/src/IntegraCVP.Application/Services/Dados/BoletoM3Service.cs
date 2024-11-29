c5205561e6564d8769584d116be1fd0d1b8cdde5
Eliezer <eliezerjs@live.com>
2024-11-29 08:19:26 -0300

    ajuste em layout.

--- IntegraCVP/src/IntegraCVP.Application/Services/Dados/BoletoM3Service.cs
+++ IntegraCVP/src/IntegraCVP.Application/Services/Dados/BoletoM3Service.cs
@@ -8,28 +8,42 @@ namespace IntegraCVP.Application.Services
         {
             return new List<(string Key, float X, float Y, float FontSize)>
             {
-                ("AGENCIA", 47, 186, 8),
-                ("APOLICE", 138, 186, 8),
-                ("FATURA", 210, 186, 8),
-                ("PERIODO", 282, 186, 8),
-                ("EMISSAO", 409, 186, 8),
-                ("VENCIMENT", 484, 186, 8),
-                ("ESTIPULANTE", 47, 217, 8),
-                ("CNPJ1", 340, 217, 8),
-                ("ENDERE1", 47, 251, 8),
-                ("CEP1", 47, 284, 8),
-                ("CIDADE1", 157, 284, 8),
-                ("EST1", 488, 284, 8),
-                ("SUBESTIPULANTE", 47, 317, 8),
-                ("CNPJ2", 396, 317, 8),
-                ("ENDERE2", 47, 350, 8),
-                ("CEP2", 47, 385, 8),
-                ("CIDADE2", 156, 385, 8),
-                ("EST2", 480, 385, 8),
-                ("NVIDAS", 47, 418, 8),
-                ("CAPITAL", 156, 418, 8),
-                ("IOF", 312, 418, 8),
-                ("PREMIO", 400, 418, 8)
+                //inicio linha 1
+                ("AGENCIA", 48, 182, 9),
+                ("APOLICE", 138, 182, 9),
+                ("FATURA", 215, 182, 9),
+                ("PERIODO", 282, 182, 9),
+                ("EMISSAO", 410, 182, 9),
+                ("VENCIMENT", 486, 182, 9),
+                //fim
+
+                //inicio linha 2
+                ("ESTIPULANTE", 48, 215, 9),
+                ("CNPJ1", 346, 215, 9),
+
+                //inicio linha 3
+                ("ENDERE1", 48, 249, 9),
+
+                //inicio linha 4
+                ("CEP1", 47, 281, 9),
+                ("CIDADE1", 159, 281, 9),
+                ("EST1", 499, 281, 9),
+
+                //inicio linha 5
+                ("SUBESTIPULANTE", 47, 314, 8),
+                ("CNPJ2", 406, 314, 9),
+
+                //inicio linha 6
+                ("ENDERE2", 47, 350, 9),
+                ("CEP2", 49, 382, 9),
+                ("CIDADE2", 159, 382, 9),
+                ("EST2", 487, 382, 9),
+
+                //inicio linha 7
+                ("NVIDAS", 49, 415, 9),
+                ("CAPITAL", 160, 415, 9),
+                ("IOF", 312, 415, 9),
+                ("PREMIO", 412, 415, 9)
             };
         }
 
