01718c5a0847c1d619bbe22f73158b9de2d70308
Eliezer <eliezerjs@live.com>
2025-01-02 03:50:08 -0300

    Inclusão de ajuste de campos prev m1.

--- IntegraCVP/src/IntegraCVP.Application/Services/Dados/PrevidenciaM1Service.cs
+++ IntegraCVP/src/IntegraCVP.Application/Services/Dados/PrevidenciaM1Service.cs
@@ -8,13 +8,10 @@ namespace IntegraCVP.Application.Services
         {
             return new List<(string Key, float X, float Y, float FontSize, bool isBold)>
             {
-                ("BOLETO_DT_VENCTO",190, 477, 8, false),
-
-                ("BOLETO_DS_TEXTO", 70, 490, 8, false),
-                ("BOLETO_NO_CERTIFICADO", 320, 490, 8, false),
-
-                ("BOLETO_VENCIMENTO", 155, 645, 8, false)
-
+                ("PRODUTO_DT_CONC_BENEF",190, 477, 8, false),
+                ("PRODUTO_NM_PRODUTO", 70, 490, 8, false),
+                ("PRODUTO_NO_CERTIFICADO", 320, 490, 8, false),
+                ("PRODUTO_CD_PROC_SUSEP_COB_BASICA", 70, 505, 8, false)
             };
         }
 
@@ -22,12 +19,10 @@ namespace IntegraCVP.Application.Services
         {
             return new List<(string Key, float X, float Y, float FontSize, bool isBold)>
             {
-                ("BOLETO_DT_VENCTO",190, 513, 8, false),
-
-                ("BOLETO_DS_TEXTO", 70, 525, 8, false),
-                ("BOLETO_NO_CERTIFICADO", 320, 525, 8, false),
-
-                ("BOLETO_VENCIMENTO", 120, 657, 8, false)
+                ("PRODUTO_DT_CONC_BENEF",190, 513, 8, false),
+                ("PRODUTO_NM_PRODUTO", 70, 525, 8, false),
+                ("PRODUTO_NO_CERTIFICADO", 320, 525, 8, false),
+                ("PRODUTO_CD_PROC_SUSEP_COB_BASICA", 70, 540, 8, false)
             };
         }
 
@@ -35,12 +30,10 @@ namespace IntegraCVP.Application.Services
         {
             return new List<(string Key, float X, float Y, float FontSize, bool isBold)>
             {
-                ("BOLETO_DT_VENCTO",190, 535, 8, false),
-
-                ("BOLETO_DS_TEXTO", 80, 546, 8, false),
-                ("BOLETO_NO_CERTIFICADO", 320, 546, 8, false),
-
-                ("BOLETO_VENCIMENTO", 160, 703, 8, false)
+                ("PRODUTO_DT_CONC_BENEF",190, 535, 8, false),
+                ("PRODUTO_NM_PRODUTO", 77, 547, 8, false),
+                ("PRODUTO_NO_CERTIFICADO", 320, 547, 8, false),
+                ("PRODUTO_CD_PROC_SUSEP_COB_BASICA", 77, 563, 8, false)
             };
         }
 
@@ -48,12 +41,10 @@ namespace IntegraCVP.Application.Services
         {
             return new List<(string Key, float X, float Y, float FontSize, bool isBold)>
             {
-                ("BOLETO_DT_VENCTO",190, 529, 8, false),
-
-                ("BOLETO_DS_TEXTO", 75, 540, 8, false),
-                ("BOLETO_NO_CERTIFICADO", 330, 540, 8, false),
-
-                ("BOLETO_VENCIMENTO", 160, 693, 8, false)
+                ("PRODUTO_DT_CONC_BENEF",190, 529, 8, false),
+                ("PRODUTO_NM_PRODUTO", 75, 540, 8, false),
+                ("PRODUTO_NO_CERTIFICADO", 330, 540, 8, false),
+                ("PRODUTO_CD_PROC_SUSEP_COB_BASICA", 75, 555, 8, false)
             };
         }
 
@@ -61,12 +52,10 @@ namespace IntegraCVP.Application.Services
         {
             return new List<(string Key, float X, float Y, float FontSize, bool isBold)>
             {
-                ("BOLETO_DT_VENCTO",190, 450, 8, false),
-
-                ("BOLETO_DS_TEXTO", 80, 462, 8, false),
-                ("BOLETO_NO_CERTIFICADO", 320, 462, 8, false),
-
-                ("BOLETO_VENCIMENTO", 160, 641, 8, false)
+                ("PRODUTO_DT_CONC_BENEF",190, 450, 8, false),
+                ("PRODUTO_NM_PRODUTO", 75, 462, 8, false),
+                ("PRODUTO_NO_CERTIFICADO", 320, 462, 8, false),
+                ("PRODUTO_CD_PROC_SUSEP_COB_BASICA", 75, 477, 8, false),
             };
         }
 
@@ -74,12 +63,10 @@ namespace IntegraCVP.Application.Services
         {
             return new List<(string Key, float X, float Y, float FontSize, bool isBold)>
             {
-                ("BOLETO_DT_VENCTO",190, 487, 8, false),
-
-                ("BOLETO_DS_TEXTO", 80, 497, 8, false),
-                ("BOLETO_NO_CERTIFICADO", 320, 498, 8, false),
-
-                ("BOLETO_VENCIMENTO", 170, 643, 8, false)
+                ("PRODUTO_DT_CONC_BENEF",190, 487, 8, false),
+                ("PRODUTO_NM_PRODUTO", 75, 499, 8, false),
+                ("PRODUTO_NO_CERTIFICADO", 320, 499, 8, false),
+                ("PRODUTO_CD_PROC_SUSEP_COB_BASICA", 75, 514, 8, false),
             };
         }
 
@@ -87,12 +74,10 @@ namespace IntegraCVP.Application.Services
         {
             return new List<(string Key, float X, float Y, float FontSize, bool isBold)>
             {
-                ("BOLETO_DT_VENCTO",190, 527, 8, false),
-
-                ("BOLETO_DS_TEXTO", 80, 5439, 8, false),
-                ("BOLETO_NO_CERTIFICADO", 320, 539, 8, false),
-
-                ("BOLETO_VENCIMENTO", 160, 659, 8, false)
+                ("PRODUTO_DT_CONC_BENEF",190, 527, 8, false),
+                ("PRODUTO_NM_PRODUTO", 75, 540, 8, false),
+                ("PRODUTO_NO_CERTIFICADO", 320, 540, 8, false),
+                ("PRODUTO_CD_PROC_SUSEP_COB_BASICA", 75, 555, 8, false),
             };
         }
 
@@ -100,12 +85,10 @@ namespace IntegraCVP.Application.Services
         {
             return new List<(string Key, float X, float Y, float FontSize, bool isBold)>
             {
-                ("BOLETO_DT_VENCTO",190, 496, 8, false),
-
-                ("BOLETO_DS_TEXTO", 80, 507, 8, false),
-                ("BOLETO_NO_CERTIFICADO", 320, 507, 8, false),
-
-                ("BOLETO_VENCIMENTO", 160, 652, 8, false)
+                ("PRODUTO_DT_CONC_BENEF",190, 496, 8, false),
+                ("PRODUTO_NM_PRODUTO", 77, 508, 8, false),
+                ("PRODUTO_NO_CERTIFICADO", 320, 508, 8, false),
+                ("PRODUTO_CD_PROC_SUSEP_COB_BASICA", 77, 525, 8, false),
             };
         }
 
@@ -113,12 +96,10 @@ namespace IntegraCVP.Application.Services
         {
             return new List<(string Key, float X, float Y, float FontSize, bool isBold)>
             {
-                 ("BOLETO_DT_VENCTO",100, 532, 8, false),
-
-                ("BOLETO_DS_TEXTO", 80, 543, 8, false),
-                ("BOLETO_NO_CERTIFICADO", 320, 543, 8, false),
-
-                ("BOLETO_VENCIMENTO", 160, 712, 8, false)
+                ("PRODUTO_DT_CONC_BENEF",100, 532, 8, false),
+                ("PRODUTO_NM_PRODUTO", 77, 544, 8, false),
+                ("PRODUTO_NO_CERTIFICADO", 320, 544, 8, false),
+                ("PRODUTO_CD_PROC_SUSEP_COB_BASICA", 77, 559, 8, false),
             };
         }
 
